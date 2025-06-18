b64599ebdcea23b65f10ef598644f7e0a45486b1
Prawin002 <prawinkumar741@gmail.com>
2025-06-18 19:00:36 +0530

    V2toABStemp.py

--- V2toABStemp.py
+++ V2toABStemp.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from io import BytesIO
 import openpyxl
+print("✅ openpyxl imported successfully")
 
 st.set_page_config(page_title="ERP Column Mapper", layout="centered")
 st.title("ERP Column Mapper and Transfer Tool")
